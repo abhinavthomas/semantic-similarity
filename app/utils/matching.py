2f381901326a2bcefd69ea9a35647ed61836685b
Abhinav Thomas <abhinavthomas@gmail.com>
2021-08-06 20:40:17 +0100

    Added license

--- app/utils/matching.py
+++ app/utils/matching.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+# Semantic similarity implementation.
+
+# Provides a fast api,for semantic similarity search using Spotify Annoy and Universal Sentence Encoder.
+
+# Copyright (c) Abhinav Thomas.
+
+# semantic-similarity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# semantic similarity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with semantic-similarity.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging
 import os
 import pickle
